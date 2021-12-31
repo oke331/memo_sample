814ee00f838afebc113c65d331dc97c9b0c49319
Takeo Yukihiro <takeo.takeo331@gmail.com>
2021-12-31 17:00:04 +0900

    Add memo_list_body

---
+++ lib/presentation/page/memo_list_page/memo_list_body.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:memo_sample/infrastructure/model/memo.dart';
+import 'package:memo_sample/presentation/controller/auth_controller/auth_controller.dart';
+
+class MemoListBody extends HookConsumerWidget {
+  const MemoListBody({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final uid = ref
+        .watch(authControllerProvider.select((value) => value.firebaseUser))
+        .value!
+        .uid;
+    return FirestoreListView<Memo>(
+      query: FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .collection('memos')
+          .orderBy('updatedAt')
+          .withConverter<Memo>(
+            fromFirestore: (snapshot, _) => Memo.fromDocumentSnapshot(snapshot),
+            toFirestore: (obj, _) => obj.toJson(),
+          ),
+      itemBuilder: (context, snapshot) {
+        final memo = snapshot.data();
+        return Text(
+          memo.title.isNotEmpty ? memo.title : memo.text,
+          overflow: TextOverflow.ellipsis,
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/page/memo_list_page/memo_list_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MemoListTile extends HookConsumerWidget {
+  const MemoListTile({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
