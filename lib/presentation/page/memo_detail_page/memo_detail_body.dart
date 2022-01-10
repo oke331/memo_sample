8ee5e412ed129365fd434f71e767a08850c779a1
Takeo Yukihiro <takeo.takeo331@gmail.com>
2022-01-10 20:23:00 +0900

    Fix navigating to memo edit page

--- lib/presentation/page/memo_detail_page/memo_detail_body.dart
+++ lib/presentation/page/memo_detail_page/memo_detail_body.dart
@@ -3,25 +3,30 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memo_sample/presentation/page/memo_detail_page/memo_detail_page.dart';
 import 'package:memo_sample/presentation/page/memo_detail_page/memo_detail_text_section.dart';
 import 'package:memo_sample/presentation/page/memo_detail_page/memo_detail_title_section.dart';
+import 'package:memo_sample/router.dart';
 
 class MemoDetailBody extends HookConsumerWidget {
   const MemoDetailBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final title =
-        ref.watch(memoDetailPageProvider.select((value) => value.title));
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (title.isNotEmpty) ...[
-              const MemoDetailTitleSection(),
-              const Divider(height: 1)
+    final memo = ref.watch(memoDetailPageProvider);
+    final router = ref.watch(routerProvider);
+    return GestureDetector(
+      onTap: () => router.go('/detail/${memo.id}/edit'),
+      behavior: HitTestBehavior.translucent,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (memo.title.isNotEmpty) ...[
+                const MemoDetailTitleSection(),
+                const Divider(height: 1)
+              ],
+              const MemoDetailTextSection(),
             ],
-            const MemoDetailTextSection(),
-          ],
+          ),
         ),
       ),
     );
