2c894baba5ffb44ce108b896243013616b91f4f9
Takeo Yukihiro <takeo.takeo331@gmail.com>
2022-01-20 21:04:49 +0900

    Add date_time_formatter_test.dart

---
+++ test/util/date_time_formatter_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:memo_sample/util/date_time_formatter.dart';
+
+void main() {
+  group('[FormatFromDateFormatString]', () {
+    test('Return correctly String', () {
+      const dateTimeString = '2022-01-01 10:01:00';
+      final dateTime = DateTime.parse(dateTimeString);
+      final result = formatFromDateFormatString(
+        dateTime: dateTime,
+        formatString: 'yyyy-MM-dd HH:mm:ss',
+      );
+      expect(result, equals(dateTimeString));
+    });
+  });
+}
